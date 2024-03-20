686963d75b94a1b5e03491a92dfbabe587d91a31
Appaz <matias2007play4@mgail.com>
2024-03-20 20:14:19 -0300

    Update

--- JavaScript Intermedio/Intermedio.js
+++ JavaScript Intermedio/Intermedio.js
@@ -426,5 +426,165 @@ function test(a, b = 3, c = 42) {
 const test = (a, b = 3, c = 42) => {
   return a + b + c;
 }
-console.log(test(5)); //50
+console.log(test(5)); // 50
+
+// ES6 Objects
+// JavaScript variables can be Object data types that contain many values called properties. An object can also have properties that are function definitions called methods for performing actions on the object
+let tree = {
+  height: 10,
+  color: 'green',
+  grow() {
+    this.height += 2;
+  }
+};
+tree.grow();
+console.log(tree.height); // 12
+// You can also use a property value shorthand when initializing properties with a variable by the same name
+let height = 5;
+let health = 100;
+let athlete = {
+  height,
+  health
+};
+// When creating an object by using duplicate property names, the last property will overwrite the prior ones of the same name
+var a = {x: 1, x: 2, x: 3, x: 4};
+// With ES6, you can now use computed property names. Using the square bracket notation [], we can use an expression for a property name, including concatenating strings
+let prop = 'name';
+let id = '1234';
+let mobile = '08923';
+let user = {
+  [prop]: 'Jack',
+  [`user_${id}`]: `${mobile}`
+};
+// ES6 adds a new Object method assign() that allows us to combine multiple sources into one target to create a single new object. Object.assign() is also useful for creating a duplicate of an existing object
+let human = {
+  name: 'Jack',
+  age: 18,
+  sex: 'male'
+};
+let student = {
+  name: 'Bob',
+  age: 20,
+  xp: '2'
+};
+let newStudent = Object.assign({}, human, student);
+let newStudent2 = Object.assign({}, human, student, {name:"Max"});
+// Here we used Object.assign() where the first parameter is the target object you want to apply new properties to, in this case a new object. Every parameter after the first will be used as sources for the target. There are no limitations on the number of source parameters. However, order is important because properties in the second parameter will be overridden by properties of the same name in third parameter, and so on
+// If we use assignment to create a new Object, it acts as a reference to the original, that-s why we must use Object.assign
+let person = {
+  name: 'Jack',
+  age: 18
+};
+let newPerson = Object.assign({}, person); 
+newPerson.name = 'Bob';
+console.log(person.name); // Jack
+console.log(newPerson.name); // Bob
+
+// ES6 Destructuring
+// The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables. ES6 has added a shorthand syntax for destructuring an array
+let array = ['1', '2', '3'];
+let [one, two, three] = array;
+console.log(two); // 2
+// We can use also destructure an array returned by a function
+let a = () => {
+  return [1, 3, 2];
+};
+let [uno, tres, dos] = a();
+//The destructuring syntax also simplifies assignment and swapping values
+let a, x, y = 4, d = 8;
+[a, x = 6] = [2]; // a = 2, x = 6
+[y, d] = [d, y]; // y = 8, d = 4
+// Similar to Array destructuring, Object destructuring unpacks properties into distinct variables
+let ob = {h:100, s: true};
+let {h, s} = ob;
+console.log(s); // true
+// We can assign without declaration, but there are some syntax requirements for that
+let a, o;
+({o, a} = {a: 'Hello ', o: 'Jack'});
+console.log(a + o); // Hello Jack
+// However, you can also do it as follows where the () are not required
+let {a, z} = {a: 'Hello ', z: 'Jack'};
+console.log(a + z);
+// You can also assign the object to new variable names
+var i = {h: 42, s: true};
+var {h: foo, s: bar} = i;
+// console.log(h); // Error
+console.log(foo); // 42
+console.log(bar); // true
+// Finally you can assign default values to variables, in case the value unpacked from the object is undefined
+var obje = {i: 42, name: "Jack"};
+let {i = 10, age = 20} = obje;
+console.log(i); // 42
+console.log(age); // 20
+
+// Rest and Spread
+// Prior to ES6, if we wanted to pass a variable number of arguments to a function, we could use the arguments object, an array-like object, to access the parameters passed to the function. Let's write a function that checks if an array contains all the arguments passed
+function containsAll(arr) {
+  for (let k = 1; k < arguments.length; k++) { // arguments accesses to the arguments passed to the function
+    let num = arguments[k];
+    if (arr.indexOf(num) === -1) { // indexOf() returns the index of an item and -1 if it does not exist
+      return false;
+    }
+  }
+  return true;
+}
+let x = [2, 4, 6, 7];
+console.log(containsAll(x, 2, 4, 7));
+console.log(containsAll(x, 6, 4, 9));
+// While this does the job, ES6 provides a more readable syntax to achieve variable number of parameters by using a rest parameter
+function containsAll(arr, ...nums) {
+  for (let num of nums) {
+    if (arr.indexOf(num) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+// The ...nums parameter is called a rest parameter. It takes all the "extra" arguments passed to the function and makes an array with them. The three dots (...) are called the Spread operator
+function magic(...nums) {
+  let sum = 0;
+  nums.filter(n => n % 2 == 0).map(el => sum+= el); // .filter() creates a new array with the values that respect the condition, and .map() takes one value from the array at a time to execute the function inside of it
+  return sum;
+}
+console.log(magic(1, 2, 3, 4, 5, 6)); // 12
+// The Spread Operator is similar to the Rest Parameter, but it has another purpose when used in objects or arrays or function calls (arguments)
+// It is common to pass the elements of an array as arguments to a function
+/* Prior */
+function myFunction(w, x, y, z) {
+  console.log(w + x + y + z);
+}
+var args = [1, 2, 3];
+myFunction.apply(null, args.concat(4)); // Adds an item to the end of the array
+/* ES6 */
+const myFunction = (w, x, y, z) => {
+  console.log(w + x + y + z);
+};
+let args = [1, 2, 3];
+myFunction(...args, 4);
+/*--------------*/
+var dateFields = [1970, 0, 1];  // 1 Jan 1970
+var date = new Date(...dateFields);
+console.log(date);
+// Before ES6, we used the following syntax to add an item at middle of an array
+var a = ["One", "Two", "Five"];
+a.splice(2, 0, "Three");
+a.splice(3, 0, "Four");
+// You can use methods such as push, splice, and concat, for example, to achieve this in different positions of the array. However, in ES6 the spread operator lets us do this more easily
+let newArr = ['Three', 'Four']; 
+var a = ['One', 'Two', ...newArr, 'Five'];
+console.log(a);
+// In objects it copies the own enumerable properties from the provided object onto a new object
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 5 };
+var clonedObj = {...obj1}; // { foo: "bar", x: 42 }
+var mergedObj = {...obj1, ...obj2}; // { foo: "baz", x: 42, y: 5 }
+// However, if you try to merge them you will not get the result you expected
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 5 };
+var merge = (...objects) => ({...objects});
+var mergedObj = merge(obj1, obj2);
+// { 0: { foo: 'bar', x: 42 }, 1: { foo: 'baz', y: 5 } }
+var mergedObj2 = merge({}, obj1, obj2);
+// { 0: {}, 1: { foo: 'bar', x: 42 }, 2: { foo: 'baz', y: 5 } }
+
 
