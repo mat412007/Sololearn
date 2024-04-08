0ea0d7592ddd79e9859168c531dad6b9f3cf657b
Appaz <matias2007play4@gmail.com>
2024-04-08 17:30:01 -0300

    Update

--- Introduction_Python/Introduction.py
+++ Introduction_Python/Introduction.py
@@ -2,12 +2,12 @@
 # Python is one of the most popular and useful languages used to give instructions to computers
 # Python has a simple syntax, it's easy to read, write and learn
 # The code in computer programs is made of statements, instructions for the computer to follow 
-#--------------------
+# 1---------------------
 # The print() instruction is the easiest way to display a value on the screen or any other display device
 # It requires parenthesis  and quotation marks around text messages, but not for numbers
 print("Hello World")
 print(10)
-#-------------------
+# 2-------------------
 # Computer programs use variables to store and remember important information to use later
 # To create it you give it a name and a value, both separated by the equal sign
 # You can think of a variable as a box that contains information
@@ -18,23 +18,35 @@ print(10)
 name = "Marco"
 print(name)
 name = "Diego"
-#-----------------
+# 3-----------------
 # A piece of text is called a string
 # Strings need to be surrounded by quotation marks
 # Both single '' and double "" quotes can be used to define strings, but the marks need to match
 string = "Text surrounded by quotation marks"
-#-----------------
+# 4-----------------
 # Numerical data is information that comes in the form of numbers
 # Numbers are not surrounded by quotation marks
 number = 100
 print(50)
 # You can also perform math operations with numbers
 # You can store the result of a calculation inside a variable
+# You can also make calculations using values inside variables
 Suma = 10 + 5
 Resta = 10 - 5
 Multiplicacion = 10 * 5 
 Division = 10 / 5
-# You can also make calculations using values inside variables
 print(Suma + 10)
 print(Suma + Resta)
-#----------------
+# 5----------------
+# Coding consists of 3 steps: writting, executing, debuging
+# Machines will not follow instructions if the contain mistakes
+# This mistakes in the code are called bugs
+# The computer reads and executes instructions from top to bottom, but it stops as soon as it finds a bug
+# 6----------------
+# Computer programs must have universal standars and practices for collaboration
+# - Professional developers use comments to add explanations to their programs, using the hash symbol #, their content is not read by the computer
+# - Python is a case-sensitive language, meaning that 'A' and 'a' are treated as different
+# - Snake case is a popular way to create variable names using underscores _ to separate words in the variable name
+# - A variable name can contain numbers, but can't start with a number
+# 7-----------------
+# 
