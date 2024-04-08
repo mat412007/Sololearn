d53663f3c54b39b0549bcfdf0a86a0c107b8617c
Appaz <matias2007play4@gmail.com>
2024-04-08 17:07:04 -0300

    Update

---
+++ Introduction_Python/Introduction.py
@@ -0,0 +1,40 @@
+# PYTHON
+# Python is one of the most popular and useful languages used to give instructions to computers
+# Python has a simple syntax, it's easy to read, write and learn
+# The code in computer programs is made of statements, instructions for the computer to follow 
+#--------------------
+# The print() instruction is the easiest way to display a value on the screen or any other display device
+# It requires parenthesis  and quotation marks around text messages, but not for numbers
+print("Hello World")
+print(10)
+#-------------------
+# Computer programs use variables to store and remember important information to use later
+# To create it you give it a name and a value, both separated by the equal sign
+# You can think of a variable as a box that contains information
+# Always give the variables descriptive names, so you know what they have stored
+# Variables are key for software development, they allow you to store, lbael and play with data
+# You can access the value a variable is storing by calling it's name
+# You can update the value of a variable and the variable will forget the previous value, this is called reassigning
+name = "Marco"
+print(name)
+name = "Diego"
+#-----------------
+# A piece of text is called a string
+# Strings need to be surrounded by quotation marks
+# Both single '' and double "" quotes can be used to define strings, but the marks need to match
+string = "Text surrounded by quotation marks"
+#-----------------
+# Numerical data is information that comes in the form of numbers
+# Numbers are not surrounded by quotation marks
+number = 100
+print(50)
+# You can also perform math operations with numbers
+# You can store the result of a calculation inside a variable
+Suma = 10 + 5
+Resta = 10 - 5
+Multiplicacion = 10 * 5 
+Division = 10 / 5
+# You can also make calculations using values inside variables
+print(Suma + 10)
+print(Suma + Resta)
+#----------------
