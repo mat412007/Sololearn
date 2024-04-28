dcebc2176cc3f37eaff7767288f768fbac94b897
Appaz <matias2007play4@gmail.com>
2024-04-28 01:13:13 -0300

    Update

--- Intermediate_Python/Practice.py
+++ Intermediate_Python/Practice.py
@@ -1,14 +1,19 @@
 print()
-def uppercase(func):
-    def wrapper():
-        message = func()
-        modified_message = message.upper()
-        return modified_message
-    return wrapper()
+class Car:
+    def __init__(self, model, year, odometer):
+        self.model = model
+        self.year = year
+        self.__odometer = odometer
+        
+    def describe_car(self):
+        print(self.year, self.model)
+        
+    def read_odometer(self):
+        print("Odometer:", self.__odometer, "miles")
 
-@uppercase 
-def greet():
-    return "Welcome!"
-print(greet)
+my_car = Car('Audi', 2020, 15000)
 
+my_car.describe_car()
+my_car.read_odometer()
+print(my_car._Car__odometer)
 print()
