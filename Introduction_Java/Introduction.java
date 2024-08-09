44393592216677e4437c8f435742522c592709ec
Appaz <matias2007play4@gmail.com>
2024-08-09 20:57:37 -0300

    Start

---
+++ Introduction_Java/Introduction.java
@@ -0,0 +1,29 @@
+/* 
+Java es uno de los lenguajes de programación más populares.
+El lema de Java es "Write once, run anywhere". 
+Los programas Java pueden ejecutarse en distintas plataformas, como móviles, ordenadores y otros sistemas portátiles. 
+Puedes utilizar Java para crear apps, juegos, aplicaciones bancarias, aplicaciones web y mucho más. 
+*/
+/* 
+Los humanos utilizamos programas informáticos para comunicarnos con las máquinas. 
+Sin programas informáticos, no tendríamos smartphones, páginas web ni exploración del espacio exterior.
+La mayoría de los programas informáticos están diseñados para producir salida, como un mensaje en la pantalla.
+Los programadores usan salidas todo el tiempo para comprobar que el ordenador sigue las instrucciones dadas y solucionar problemas con el código.
+*/
+/*
+java -version 
+javac -version 
+javac -target 8 -source 8 file.java / javac --release 8 introduction.java
+java class_name
+*/
+// -----------------------------------------------------------
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("");
+        
+        System.out.println("Hello");
+
+        System.out.println("");
+    }
+}
+
